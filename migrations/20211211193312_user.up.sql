726c03dad66cc530f6aaee691b30dacc1b485132
Jakub Blaszczyk <jablaszczyk@hotmail.com>
2021-12-12 17:56:25 +0100

    Add actix graphql and sqlx initial commit

---
+++ migrations/20211211193312_user.up.sql
@@ -0,0 +1,8 @@
+-- Add up migration script here
+CREATE TABLE Account (
+  id    UUID NOT NULL DEFAULT gen_random_uuid(),
+  login         varchar(255) NOT NULL,
+  password    varchar(255) NOT NULL,
+  privileges varchar(255) NOT NULL,
+  CONSTRAINT id
+    PRIMARY KEY (id));
---
+++ src/model.rs
@@ -0,0 +1,89 @@
+use anyhow::Result;
+use async_graphql::SimpleObject;
+use serde::{Deserialize, Serialize};
+use sqlx::{FromRow, PgPool};
+use uuid::Uuid;
+
+/*
+CREATE TABLE Account (
+  id    UUID NOT NULL DEFAULT gen_random_uuid(),
+  login         varchar(255) NOT NULL,
+  password    varchar(255) NOT NULL,
+  privileges varchar(255) NOT NULL,
+  CONSTRAINT id
+    PRIMARY KEY (id));
+*/
+
+#[derive(SimpleObject, FromRow, Deserialize, Serialize)]
+pub struct Account {
+    pub id: uuid::Uuid,
+    login: String,
+    password: String,
+    privileges: String,
+}
+
+impl Account {
+
+    pub async fn create(
+        pool: &PgPool,
+        login: &str,
+        password: &str,
+        privileges: &str,
+    ) -> Result<Account> {
+        let row = sqlx::query!(
+            "INSERT INTO account(Login, password, privileges) VALUES ($1, $2, $3) RETURNING id",
+            login,
+            password,
+            privileges
+        )
+        .fetch_one(pool)
+        .await?;
+
+        Ok(Account {
+            id: row.id,
+            login: login.to_string(),
+            password: password.to_string(),
+            privileges: privileges.to_string(),
+        })
+    }
+
+    pub async fn read_one(pool: &PgPool, id: &str) -> Result<Account> {
+        let row = sqlx::query_as!(
+            Account,
+            "SELECT * FROM account WHERE id = $1",
+            Uuid::parse_str(id)?
+        )
+        .fetch_one(pool)
+        .await?;
+
+        Ok(row)
+    }
+
+    pub async fn read_all(pool: &PgPool) -> Result<Vec<Account>> {
+        let rows = sqlx::query_as!(Account, "SELECT * FROM account")
+            .fetch_all(pool)
+            .await?;
+
+        Ok(rows)
+    }
+
+    pub async fn update(pool: &PgPool, id: &str, password: &str) -> Result<Account> {
+        sqlx::query!(
+            "UPDATE account SET password=$1 WHERE id = $2",
+            password,
+            Uuid::parse_str(id)?
+        )
+        .execute(pool)
+        .await?;
+
+        Ok(Account::read_one(pool, id).await?)
+    }
+
+    pub async fn delete(pool: &PgPool, id: &str) -> Result<()> {
+        sqlx::query!("DELETE FROM account WHERE id = $1", Uuid::parse_str(id)?)
+            .execute(pool)
+            .await?;
+
+        Ok(())
+    }
+}
