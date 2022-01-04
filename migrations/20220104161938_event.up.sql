697b66286f5f06ce06b21d99a6f82faa4655f81b
Jakub Blaszczyk <jablaszczyk@hotmail.com>
2022-01-04 17:24:03 +0100

    Move migrations to global file

---
+++ migrations/20220104161938_event.up.sql
@@ -0,0 +1,8 @@
+-- Add up migration script here
+CREATE TABLE EVENT (
+  ID              SERIAL NOT NULL, 
+  ID_apiary       int4 NOT NULL, 
+  Time_start      timestamp, 
+  Time_end        timestamp, 
+  note            varchar(1023), 
+  PRIMARY KEY (ID));
