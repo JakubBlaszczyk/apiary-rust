f3b598122fcaae981cb898ce021d864dd9e5ba5d
Jakub Blaszczyk <jablaszczyk@hotmail.com>
2022-01-13 18:29:56 +0100

    Add sqlx automatic installation

--- backend/server/run.sh
+++ backend/server/run.sh
@@ -1,3 +1,4 @@
+cargo install --list | grep sqlx || cargo install sqlx
 docker port postgres-db || docker run --rm --name postgres-db -p 5432:5432 -e POSTGRES_PASSWORD=mypassword -d postgres
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${script_dir}
@@ -12,6 +13,6 @@ do
   cd ..
 done
 cd server
-sleep 30
+sleep 15
 rover supergraph compose --config ./supergraph-config.yaml > supergraph.graphql
 echo "Done"
